8ccfc6206017135f60b98878fdf58466a69afa29
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-11-08 04:46:20 +0600

    upd

--- project_4/project_4/form.py
+++ project_4/project_4/form.py
@@ -3,6 +3,10 @@ from django import forms
 
 class contactForm(forms.Form):  # inherit from forms.Form
     name=forms.CharField() # field of field
-    email=forms.CharField()
+    email=forms.EmailField()
     contact=forms.CharField()
-    
+    age=forms.IntegerField()
+    birthday=forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    CHOICE=[('B','BMW'),('M','Mercedez'),('T','Toyota')]
+    vehicle=forms.ChoiceField(choices=CHOICE)
+    dateTimeField=forms.DateTimeField(widget=forms.DateInput(attrs={'type':'datetime-local'}))
